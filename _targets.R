b0ac566c3a58eb7d066c988995f0094b35852e54
Saúl Gámez <saul@movimentar.eu>
2024-05-21 11:41:04 +0200

    use tarchetypes package used for automatic rendering of report in pipeline

--- _targets.R
+++ _targets.R
@@ -5,7 +5,7 @@
 
 # Load packages required to define the pipeline:
 library(targets)
-# library(tarchetypes) # Load other packages as needed.
+library(tarchetypes)
 
 # Set target options:
 tar_option_set(
