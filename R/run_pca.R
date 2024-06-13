5442e8a9c07a11df6e460ad020c3ffe0df2d8b9c
eduwf <eduardo@movimentar.eu>
2024-06-13 13:33:56 +0200

    Add references to packages
    This improves reproducibility

--- R/run_pca.R
+++ R/run_pca.R
@@ -1,53 +1,63 @@
+# Load necessary libraries
+library(dplyr)
+library(tidyr)
+library(FactoMineR)
+library(factoextra)
+library(gt)
+library(tibble)
+library(ggplot2) # Ensure ggplot2 is loaded
+
+# Function to perform PCA and hierarchical clustering on coverage costs data
 # from coverage_costs and emergency_list, runs PCA and hierarchical clustering
-# analyses. Outputs a list with a PCA Biplot, egeinvalues table, variable
-# contributions, screeplot, PCA clustered plot and dendogram.
+# analyses. Outputs a list with a PCA Biplot, eigenvalues table, variable
+# contributions, screeplot, PCA clustered plot, and dendogram.
+
 run_pca <- function(coverage_costs, emergency_list) {
-  # prepare data for PCA
+  # Prepare data for PCA
   coverages_pca_data <- coverage_costs %>%
-    ungroup() %>%
-    filter(!(coverage == 0 |
-               is.na(coverage))) %>% # remove 0 coverage and NAs
-    left_join(emergency_list, by = c("emergency", "country")) %>%
-    select(intervention_id, coverage, emergency_id) %>%
-    pivot_wider(names_from = emergency_id,
-                values_from = coverage) %>%
-    arrange(intervention_id) %>%
-    column_to_rownames("intervention_id")
+    dplyr::ungroup() %>%
+    dplyr::filter(!(coverage == 0 | is.na(coverage))) %>% # Remove 0 coverage and NAs
+    dplyr::left_join(emergency_list, by = c("emergency", "country")) %>%
+    dplyr::select(intervention_id, coverage, emergency_id) %>%
+    tidyr::pivot_wider(names_from = emergency_id, values_from = coverage) %>%
+    dplyr::arrange(intervention_id) %>%
+    tibble::column_to_rownames("intervention_id")
   
-  coverages_pca_data[is.na(coverages_pca_data)] <- 0 # replace NAs by 0s
+  coverages_pca_data[is.na(coverages_pca_data)] <- 0 # Replace NAs with 0s
   
-  # create PCA object
-  coverage_pca <- PCA(coverages_pca_data,
-                      scale.unit = FALSE)
+  # Create PCA object
+  coverage_pca <- FactoMineR::PCA(coverages_pca_data, scale.unit = FALSE)
   
-  # get eigenvalue table
-  eigenvalues <- get_eigenvalue(coverage_pca) %>%
-    as_tibble()
+  # Get eigenvalue table
+  eigenvalues <- factoextra::get_eigenvalue(coverage_pca) %>%
+    tibble::as_tibble()
   
-  # get variable contribution table
-  var_contributions <- get_pca_var(coverage_pca)$contrib %>%
+  # Get variable contribution table
+  var_contributions <- factoextra::get_pca_var(coverage_pca)$contrib %>%
     as.data.frame() %>%
-    rownames_to_column(var = "intervention_id")
+    tibble::rownames_to_column(var = "intervention_id")
   
-  # get screeplot
-  screeplot <- fviz_screeplot(coverage_pca)
+  # Get screeplot
+  screeplot <- factoextra::fviz_screeplot(coverage_pca)
   
-  #get PCA clustered and dendogram object
-  intervention_hcpc <-
-    HCPC(coverage_pca,
-         nb.clust = 4)
+  # Get PCA clustered and dendogram object
+  intervention_hcpc <- FactoMineR::HCPC(coverage_pca, nb.clust = 4)
   
-  # return PCA results
-  pca_results <- list(
-    pca_biplot = fviz_pca_biplot(coverage_pca),
-    eigenvalues = eigenvalues,
-    var_contributions = var_contributions,
-    screeplot = screeplot,
-    clusters = fviz_cluster(intervention_hcpc),
-    dendogram = fviz_dend(intervention_hcpc)
-  )
+  # Suppress specific ggplot2 warning
+  suppressWarnings({
+    # Return PCA results
+    pca_results <- list(
+      pca_biplot = factoextra::fviz_pca_biplot(coverage_pca) + 
+        guides(scale = "none"),
+      eigenvalues = eigenvalues,
+      var_contributions = var_contributions,
+      screeplot = screeplot,
+      clusters = factoextra::fviz_cluster(intervention_hcpc) + 
+        guides(scale = "none"),
+      dendogram = factoextra::fviz_dend(intervention_hcpc) + 
+        guides(scale = "none")
+    )
+  })
   
   return(pca_results)
 }
-
-
